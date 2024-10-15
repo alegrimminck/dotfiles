e0066ad06952a211150f13ac08aef96a8ec2bda1
alegrimminck <alegrimminck@gmail.com>
2024-10-15 10:48:56 -0300

    add pulso functions

--- zsh/pulso_scripts.sh
+++ zsh/pulso_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-resurrect_pulso() {
+function resurrect_pulso() {
   # Layout
   create_right
   resize_right
@@ -21,89 +21,100 @@ resurrect_pulso() {
   run_admin_dashboard
   down
   run_docker
+
+
+  run_postgres() {
+    slow_sleep
+    xdotool type --delay 36 "echo <password> | sudo -S systemctl restart postgresql" && xdotool key Return
+    slow_sleep
+  }
+
+  run_pulso_app_server() {
+    slow_sleep
+    xdotool type --delay 36 "cd /home/dev/pulso/pulso-app/" && xdotool key Return
+    slow_sleep
+    xdotool type --delay 36 "rails s -b 192.168.101.1" && xdotool key Return
+  }
+
+  run_pulso_app_webpacker() {
+    slow_sleep
+    xdotool type --delay 36 "cd /home/dev/pulso/pulso-app/" && xdotool key Return
+    slow_sleep
+    xdotool type --delay 36 "bin/webpacker-dev-server" && xdotool key Return
+    slow_sleep
+  }
+
+  run_admin_dashboard() {
+    slow_sleep
+    xdotool type --delay 36 "cd /home/dev/pulso/pulso-admin-dashboard-client" && xdotool key Return
+    slow_sleep
+    xdotool type --delay 36 "npm start" && xdotool key Return
+    slow_sleep
+  }
+
+  run_docker(){
+    slow_sleep
+    xdotool type --delay 36 "cd /home/dev/pulso" && xdotool key Return
+    slow_sleep
+    xdotool type --delay 36 "docker compose up" && xdotool key Return
+    slow_sleep
+  }
+
+  fast_sleep() {
+    sleep 0.2
+  }
+
+  slow_sleep() {
+    sleep 0.5
+  }
+
+  up() {
+    xdotool key alt+Up
+    fast_sleep
+  }
+
+  left() {
+    xdotool key alt+Left
+    fast_sleep
+  }
+
+  right() {
+    xdotool key alt+Right
+    fast_sleep
+  }
+
+  down() {
+    xdotool key alt+Down
+    fast_sleep
+  }
+
+  create_right() {
+    xdotool key ctrl+p
+    fast_sleep
+    xdotool key r
+    left
+  }
+
+  create_down() {
+    xdotool key ctrl+p
+    fast_sleep
+    xdotool key d
+    up
+  }
+
+  resize_right() {
+    xdotool key ctrl+n
+    fast_sleep
+    xdotool key Right key Right key Right
+  }
 }
 
-run_postgres() {
-  slow_sleep
-  xdotool type --delay 24 "echo <password> | sudo -S systemctl restart postgresql" && xdotool key Return
-  slow_sleep
+function restart_pulso_function() {
+  local pulso_function=$1
+  docker compose restart "$pulso_function" pubsub && docker compose restart pubsub-client
 }
 
-run_pulso_app_server() {
-  slow_sleep
-  xdotool type --delay 24 "cd /home/dev/pulso/pulso-app/" && xdotool key Return
-  slow_sleep
-  xdotool type --delay 24 "rails s -b 192.168.101.1" && xdotool key Return
-}
-
-run_pulso_app_webpacker() {
-  slow_sleep
-  xdotool type --delay 24 "cd /home/dev/pulso/pulso-app/" && xdotool key Return
-  slow_sleep
-  xdotool type --delay 24 "bin/webpacker-dev-server" && xdotool key Return
-  slow_sleep
-}
-
-run_admin_dashboard() {
-  slow_sleep
-  xdotool type --delay 24 "cd /home/dev/pulso/pulso-admin-dashboard-client" && xdotool key Return
-  slow_sleep
-  xdotool type --delay 24 "npm start" && xdotool key Return
-  slow_sleep
-}
-
-run_docker(){
-  slow_sleep
-  xdotool type --delay 24 "cd /home/dev/pulso" && xdotool key Return
-  slow_sleep
-  xdotool type --delay 24 "docker compose up" && xdotool key Return
-  slow_sleep
-}
-
-fast_sleep() {
-  sleep 0.2
-}
-
-slow_sleep() {
-  sleep 0.5
-}
-
-up() {
-  xdotool key alt+Up
-  fast_sleep
-}
-
-left() {
-  xdotool key alt+Left
-  fast_sleep
-}
-
-right() {
-  xdotool key alt+Right
-  fast_sleep
-}
-
-down() {
-  xdotool key alt+Down
-  fast_sleep
-}
-
-create_right() {
-  xdotool key ctrl+p
-  fast_sleep
-  xdotool key r
-  left
-}
-
-create_down() {
-  xdotool key ctrl+p
-  fast_sleep
-  xdotool key d
-  up
-}
-
-resize_right() {
-  xdotool key ctrl+n
-  fast_sleep
-  xdotool key Right key Right key Right
+function attach_to_pulso_function_container() {
+  local function_name=$1
+  docker attach --detach-keys="ctrl-x" pulso-"$function_name"-1
 }
