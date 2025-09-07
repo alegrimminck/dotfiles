cc98990ea74318c6fa77431ce8e8a009ec9d70ee
alegrimminck <alegrimminck@gmail.com>
2025-09-07 11:34:01 -0300

    feat(fnm): remove fnm

--- zsh/packages.sh
+++ zsh/packages.sh
@@ -1,11 +1,2 @@
 # ruby - rbenv
 eval "$(rbenv init - zsh)"
-
-# node - fnm
-FNM_PATH="/home/dev/.local/share/fnm"
-if [ -d "$FNM_PATH" ]; then
-  export PATH="/home/dev/.local/share/fnm:$PATH"
-  eval "`fnm env`"
-fi
-
-eval "$(fnm env --use-on-cd --shell zsh)"
